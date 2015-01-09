30d235b44a5bb57fe7df97ff240f752b751df0f7
LeeJunhui <84416097@qq.com>
2015-01-09 19:51:58 +0800

    Project Init

---
+++ LJHWeibo/LJHWeibo/LJHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LJHAppDelegate.h
+//  LJHWeibo
+//
+//  Created by LeeJunHui on 15/1/9.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LJHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LJHWeibo/LJHWeibo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LJHWeibo
+//
+//  Created by LeeJunHui on 15/1/9.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LJHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LJHAppDelegate class]));
+    }
+}
